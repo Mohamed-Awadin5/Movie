1ce524ce95effb3f50d3b2c00d3e92750e2aa62a
mohamed <mohamed.awadin6@gmail.com>
2025-12-03 19:43:30 +0200

    Initial Commit

---
+++ Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by mohamed awadin7 on 03/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
